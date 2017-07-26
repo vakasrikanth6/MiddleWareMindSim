2ea1e26534e9a3ce3da39aa868eed68303c63e01
tcsudarsan <tcsudarsan@gmail.com>
2017-07-26 20:05:25 +0200

    LeftAndForward.java

---
+++ LeftAndForward.java
@@ -0,0 +1,33 @@
+package coppelia;
+import coppelia.Forward;
+import coppelia.Rotate;
+import coppelia.RightAndForward;
+import coppelia.Backward;
+public class LeftAndForward extends Forward{
+	void LeftAndForward(int clientID,float speed){
+		LeftAndForward Left=new LeftAndForward(); 
+	remoteApi vrep=new remoteApi();
+	IntW leftHandle=new IntW(1);
+	IntW rightHandle=new IntW(2);
+	IntW pingTime=new IntW(3);
+	FloatWA eulerAngles=new FloatWA(1);
+	int leftHandleMotor=0;
+	int rightHandleMotor=0;
+	//leftHandleMotor=vrep.simxGetObjectHandle(clientID, "Pioneer_p3dx_leftMotor", leftHandle, vrep.simx_opmode_blocking);
+	//System.out.println(leftHandle.getValue());
+    rightHandleMotor=vrep.simxGetObjectHandle(clientID, "Pioneer_p3dx_rightMotor", rightHandle, vrep.simx_opmode_blocking);
+	//System.out.println(rightHandle.getValue());
+	//vrep.simxSetJointTargetVelocity(clientID, leftHandle.getValue(), speed*600, vrep.simx_opmode_oneshot);
+	//vrep.simxGetPingTime(clientID, pingTime);
+	vrep.simxSetJointTargetVelocity(clientID, rightHandle.getValue(), speed*600, vrep.simx_opmode_oneshot);
+	vrep.simxGetPingTime(clientID, pingTime);
+	
+	
+	try {
+	    Thread.sleep(1700);
+	} catch(InterruptedException ex) {
+	    Thread.currentThread().interrupt();
+	}
+	Left.moveForward( clientID, speed);
+	}
+}
